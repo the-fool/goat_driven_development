1d4ad80a03bff0ad7dc5a100aacc5aa28e7f9c6e
the-fool <sketchbang@gmail.com>
2016-02-12 08:28:28 -0600

    add new user test

--- lists/tests.py
+++ lists/tests.py
@@ -16,7 +16,7 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string('home.html', request=request)
         self.assertEqual(response.content.decode(), expected_html)
 
 
